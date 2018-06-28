779fa2725c6c4982998552c5c2584afb5aed45f3
Bodacious <gavin@katanacode.com>
2018-06-28 10:26:02 +0100

    Update sass generator

--- lib/generators/sass/sass_generator.rb
+++ lib/generators/sass/sass_generator.rb
@@ -1,18 +1,14 @@
 require 'rails/generators'
-
-
 class SassGenerator < Rails::Generators::NamedBase
-
 
   source_root File.expand_path('../templates', __FILE__)
 
-  desc "This generator creates a coffee script file at app/assets/stylesheets/partials"
+  desc "This generator creates a coffee script file at "\
+         "app/assets/stylesheets/partials"
 
   argument :elements, type: :array, default: [], banner: "element element"
 
-
   attr_reader :namespace
-
 
   def copy_template_file
     @namespace = class_name.split("::")
@@ -21,28 +17,20 @@ class SassGenerator < Rails::Generators::NamedBase
     template "sass.sass.erb", file_path
   end
 
-
   private
-
-
 
   def block_name
     file_name.underscore
   end
 
-
-
   def partial_file_name
     "_#{file_name}"
   end
 
-
-
   def file_path
-    Rails.root.join("app/assets/stylesheets/partials", partial_file_name + ".sass")
+    Rails.root.join("app/assets/stylesheets/blocks",
+                    partial_file_name + ".sass")
   end
-
-
 
   def namespace_path
     if namespace.blank?
@@ -52,10 +40,7 @@ class SassGenerator < Rails::Generators::NamedBase
     end
   end
 
-
-
   def singular_name
     super.singularize
   end
-
 end
